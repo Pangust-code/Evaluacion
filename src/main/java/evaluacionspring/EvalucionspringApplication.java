e76d4d64ad59792479eb8231da63be7b1e4499b7
Pangust-code <www.daniku134@gmail.com>
2026-01-09 16:33:36 -0500

    first commit

---
+++ src/main/java/evaluacionspring/EvalucionspringApplication.java
@@ -0,0 +1,13 @@
+package evaluacionspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalucionspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvalucionspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/evaluacionspring/EvalucionspringApplicationTests.java
@@ -0,0 +1,13 @@
+package evaluacionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalucionspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
